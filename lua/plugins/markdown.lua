13bb549869f90388df8194785e0a73f3774cd16f
AccursedGalaxy <robinbohrer7@gmail.com>
2026-03-17 16:02:19 +0100

    feat(plugins): markdown plugin healthcheck fixes

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,5 +3,16 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		latex = { enabled = false },
+	},
+	config = function(_, opts)
+		require("render-markdown").setup(opts)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function(ev)
+				vim.treesitter.start(ev.buf)
+			end,
+		})
+	end,
 }
