20af7cdd28c581f10a7927ef05f7e9f9015b6b05
AccursedGalaxy <robinbohrer7@gmail.com>
2026-03-18 20:58:05 +0100

    feat(telescope): add live grep with test exclusion
    Replace the default live_grep mapping with one that excludes test files
    by default, and add a new mapping for searching all files including tests.
    This improves search usability by filtering out noise from test files
    during typical grep operations.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,21 @@ return {
 	version = "*",
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").live_grep({
+					additional_args = {
+						"--glob",
+						"!**/*test*",
+						"--glob",
+						"!**/*spec*",
+					},
+				})
+			end,
+			desc = "Live grep (no tests)",
+		},
+		{ "<leader>fG", "<cmd>Telescope live_grep<cr>", desc = "Live grep (all files)" },
 	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
